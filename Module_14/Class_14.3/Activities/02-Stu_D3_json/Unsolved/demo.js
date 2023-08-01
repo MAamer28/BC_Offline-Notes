fbf778dcf8ccdc6aebb2e5223824ca37a7dcead0
MAamer28 <aamer.moe@Gmail.com>
2023-08-01 17:56:01 -0400

    Aug1_v1.0
    Updating class notes with classwork/solutions

--- Module_14/Class_14.3/Activities/02-Stu_D3_json/Unsolved/demo.js
+++ Module_14/Class_14.3/Activities/02-Stu_D3_json/Unsolved/demo.js
@@ -1,10 +1,21 @@
 // Get the Roadster endpoint
+const url2 = "https://api.spacexdata.com/v3/roadster"
 
+const dataPromise2 = d3.json(url2);
+console.log("Data Promise: ", dataPromise2);
 
 // Fetch the JSON data and console log it
-
+d3.json(url2).then(function(data2) {
+    console.log(data2);
+});
 
 // Get the capsules endpoint
+const url = "https://api.spacexdata.com/v3/capsules";
 
+const dataPromise = d3.json(url);
+console.log("Data Promise: ", dataPromise);
 
 // Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
+    console.log(data);
+});
--- Module_14/Class_14.3/Activities/04-Evr_D3_Select/Unsolved/static/js/app.js
+++ Module_14/Class_14.3/Activities/04-Evr_D3_Select/Unsolved/static/js/app.js
@@ -7,9 +7,13 @@ let newGrade = ["Wash", 79];
 // http://getbootstrap.com/docs/3.3/css/#tables-striped
 
 // Use D3 to select the table body
+let TableBody = d3.select("tbody");
 
 // Append one table row `tr` to the table body
+let student = TableBody.append("tr");
 
 // Append one cell for the student name
+let stuname = student.append("td").text(newGrade[0])
 
 // Append one cell for the student grade
+let stugrade = student.append("td").text(newGrade[1])
